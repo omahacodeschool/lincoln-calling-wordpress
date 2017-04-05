8fe8dffca3910c73647750486b671eb81cfb8f9e
Evin Grost <evin.grost@gmail.com>
2017-04-05 16:26:34 -0500

    Absolute CSS frustration.

--- static/assets/scripts.js
+++ static/assets/scripts.js
@@ -1,24 +1,30 @@
 window.addEventListener("load", function(){
 
-	function showMenu(e){
-		var menuBlock = this.childNodes[3];
-		menuBlock.style.top = menuHeight.toString() + "px";
+	// function showMenu(e){
+	// 	var menuBlock = this.childNodes[3];
+	// 	menuBlock.style.maxHeight = "auto";
 
 
 		
-		menuBlock.addEventListener("mouseout", function(){ hideMenu(menuBlock) });
-	}
+	// 	menuBlock.addEventListener("mouseout", function(){ hideMenu(menuBlock) });
+	// }
 
-	function hideMenu(menu){
-		menu.style.top = "";
-	}
+	// function hideMenu(menu){
+	// 	menu.style.top = "";
+	// }
 
 	var menuHeight = document.getElementsByClassName("js-navigationList")[0].clientHeight;
 
-	var dropDownLink = document.getElementsByClassName("js-hasDropDown");
+	var subNav = document.getElementsByClassName("js-subNavList");
 
-	for (var i = 0; i < dropDownLink.length; i++){
-		dropDownLink[i].addEventListener("mouseover", showMenu);
+	for (var i = 0; i < subNav.length; i++){
+		subNav[i].style.top = menuHeight.toString() + "px";
 	}
+
+	// var dropDownLink = document.getElementsByClassName("js-hasDropDown");
+
+	// for (var i = 0; i < dropDownLink.length; i++){
+	// 	dropDownLink[i].addEventListener("mouseover", showMenu);
+	// }
 	
 });
