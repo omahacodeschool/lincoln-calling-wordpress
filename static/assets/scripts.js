7ab8cc90a344cdbc4fd2588be8619ef40c775447
Evin Grost <evin.grost@gmail.com>
2017-04-05 13:23:02 -0500

    Struggling with z-index.

--- static/assets/scripts.js
+++ static/assets/scripts.js
@@ -1,23 +1,24 @@
 window.addEventListener("load", function(){
 
 	function showMenu(e){
-		var menuBlock = this.childNodes[1];
+		var menuBlock = this.childNodes[3];
 		menuBlock.style.top = menuHeight.toString() + "px";
+
+
 		
-		this.addEventListener("mouseout", function(){ hideMenu(menuBlock) });
+		menuBlock.addEventListener("mouseout", function(){ hideMenu(menuBlock) });
 	}
 
 	function hideMenu(menu){
 		menu.style.top = "";
 	}
 
-	var menuHeight = document.getElementsByClassName("navigationList")[0].clientHeight;
+	var menuHeight = document.getElementsByClassName("js-navigationList")[0].clientHeight;
 
-	var dropDownLink = document.getElementsByClassName("hasDropDown");
+	var dropDownLink = document.getElementsByClassName("js-hasDropDown");
 
 	for (var i = 0; i < dropDownLink.length; i++){
 		dropDownLink[i].addEventListener("mouseover", showMenu);
-		// dropDownLink[i].addEventListener("mouseout", hideMenu);
 	}
 	
 });
