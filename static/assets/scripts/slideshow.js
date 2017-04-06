0eb003675cb83b9994784688536a65aa623b14e8
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-06 12:07:57 -0500

    Finished js

--- static/assets/scripts/slideshow.js
+++ static/assets/scripts/slideshow.js
@@ -1,14 +1,23 @@
 window.addEventListener("load", function(){
 
+	document.addEventListener('visibilitychange', function () {
+	    if (document.hidden) {
+	    	clear();
+	    } else {
+	    	intSet();
+	    }
+	});
+
 	const imageStrip = document.getElementsByClassName('js-imageStrip')[0];
-	const widthOfContainer = document.getElementsByClassName('js-slideShow')[0].offsetWidth;
+	const container = document.getElementsByClassName('js-slideShow')[0];
+	const widthOfContainer = container.offsetWidth;
 	const maxPos = widthOfContainer * imageStrip.children.length;
 
 	const controls = document.getElementsByClassName("slideShowControls")[0].children;
 	const links = document.getElementsByClassName("slideShowArticles")[0].children;
 
-	imageStrip.addEventListener("mouseover", clear);
-	imageStrip.addEventListener("mouseout", intSet);
+	container.addEventListener("mouseover", clear);
+	container.addEventListener("mouseout", intSet);
 
 	var currentPos = 0;
 	var state = 0;
@@ -16,17 +25,30 @@ window.addEventListener("load", function(){
 
 	intSet();
 
+	function intSet(){
+		slideshow = setInterval( playSlideShow, 2000);
+	}
+
 	function playSlideShow(){
+		incrementSlide();
+		checkValues();
+		moveSlideShow();
+	}
+
+	function incrementSlide(){
 		currentPos += widthOfContainer;
-		state += 1;
+		state++;
+	}
+
+	function checkValues(){
 		checkPos();
 		checkstate();
-		moveStrip();
-		moveControl();
-	};
+	}
 
-	function moveStrip(){
-		imageStrip.style.right = currentPos +"px";
+	function moveSlideShow(index=state, autoOff=true){
+		var pos = index * widthOfContainer;
+		moveStrip(pos);
+		moveControl(index, autoOff);
 	}
 
 	function checkPos(){
@@ -37,24 +59,50 @@ window.addEventListener("load", function(){
 		if (state >= 4){ state = 0 };
 	}
 
-	function moveControl(){
-		controls[state].classList.toggle("slideShow__control--hover");
-		links[state].classList.toggle("slideShow__article--hover");
-		// Timeout can be glitchy
-		setTimeout( untoggle, 1900);
+	function moveStrip(pos){
+		imageStrip.style.right = pos +"px";
+	}
+
+	function moveControl(index){
+		untoggle();
+		controls[index].classList.add("slideShow__control--hover");
+		links[index].classList.add("slideShow__article--hover");
 	}
 
 	function untoggle(){
-		controls[state].classList.toggle("slideShow__control--hover");
-		links[state].classList.toggle("slideShow__article--hover");
+		for (var i = 0; i < imageStrip.children.length; i++){
+			controls[i].classList.remove("slideShow__control--hover");
+			links[i].classList.remove("slideShow__article--hover");
+		}
 	}
 
 	function clear(){
 		clearInterval(slideshow);		
 	}
 
-	function intSet(){
-		slideshow = setInterval( playSlideShow, 2000);
+	for (var i=0; i < links.length; i++){
+		links[i].addEventListener("mouseover", userToggle);
+		controls[i].addEventListener("mouseover", userToggle);
+	}
+
+	for (var i=0; i < links.length; i++){
+		links[i].addEventListener("mouseout", untoggle);
+		controls[i].addEventListener("mouseout", untoggle);
+	}
+
+	function userToggle(el){
+		var index = findElementIndex(el);
+		untoggle();
+		moveSlideShow(index);
+	}
+
+	function findElementIndex(el){
+		var children = el.target.parentElement.children;
+		for (var i=0; i < children.length; i++){
+			if (children[i] == el.target){
+				return i;
+			}
+		}
 	}
 
 });
