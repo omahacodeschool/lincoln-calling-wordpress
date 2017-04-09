9057b3f3d2769903fcb369b989787471cb61f9d7
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-09 00:58:14 -0500

    Needed to replace hard coded info with post info

--- index.php
+++ index.php
@@ -24,19 +24,29 @@
 
         <?php
         // Start the loop.
+	$args = array('post_type'=>array('ticket'));
+	query_posts($args);
+
         while ( have_posts() ) : the_post();
         ?>
 
-          <li class="passOption passOption--forSale">
-            <small class="passOption__date">April 21</small>  
+
+	<?php if (get_field('status') == "Discounted"): ?>
+		<li class="passOption passOption--forSale">
+	<?php elseif (get_field('status') == "Sold Out"): ?> 
+		<li class="passOption passOption--soldOut">
+	<?php else: ?> 
+		<li class="passOption">
+	<?php endif ?>
+
+            <small class="passOption__date">
+		<?php echo get_field('start_date'); echo get_field('end_date')?>
+	    </small>  
             <h3 class="passOption__title">
-              Early Bird <i class="fa fa-minus"></i> $20 
-              <span class="passOption__note">(Limited availability)</span>
+              <?php the_title(); ?> <i class="fa fa-minus"></i> $<?php echo get_field('price'); ?>
+              <span class="passOption__note"> <?php the_content(); ?> </span>
             </h3>
           </li>
-
-          <?php // the_title(); ?>
-          <?php // the_content(); ?>
 
         <?php
         // End the loop.
