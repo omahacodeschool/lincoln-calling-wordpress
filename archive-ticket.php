b151e6942d069f8373cd95677dce03b49b311cda
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-10 11:54:26 -0500

    Ticket archive

--- archive-ticket.php
+++ archive-ticket.php
@@ -5,11 +5,26 @@
 		while ( have_posts() ) : the_post();
 ?>
 
-			<p class="passOption__date">hello</p>
+<?php if (get_field('status') == "Discounted"): ?>
+	<li class="passOption passOption--forSale">
+<?php elseif (get_field('status') == "Sold Out"): ?> 
+	<li class="passOption passOption--soldOut">
+<?php else: ?> 
+	<li class="passOption">
+<?php endif ?>
+
+		<small class="passOption__date">
+			<?php echo get_field('start_date'); echo get_field('end_date')?>
+		</small>  
+		<h3 class="passOption__title">
+			<?php the_title(); ?><i class="fa fa-minus"></i>$<?php echo get_field('price'); ?>
+			<span class="passOption__note"><?php the_content(); ?></span>
+		</h3>
+	</li>
 
 <?php
-        endwhile;
-    endif; 
+    endwhile;
+endif; 
 ?>
 
 <?php get_footer(); ?>
