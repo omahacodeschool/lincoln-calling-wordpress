0d7dd917969249bd8bb156a1fb003e72b3d06faa
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-11 06:56:43 -0500

    Fixed indentation

--- archive-ticket.php
+++ archive-ticket.php
@@ -4,32 +4,35 @@
     <h3 class="centerText__title">Tickets</h3>
   </div>
 
-<ul>
-<?php 
-	if ( have_posts() ) : 
-		while ( have_posts() ) : the_post();
-?>
+	<div class="passOptions">
+		<ul>
+			<?php if ( have_posts() ) : 
+				while ( have_posts() ) : the_post();
+				?>
 
-<?php if (get_field('status') == "Discounted"): ?>
-	<li class="passOption passOption--forSale">
-<?php elseif (get_field('status') == "Sold Out"): ?> 
-	<li class="passOption passOption--soldOut">
-<?php else: ?> 
-	<li class="passOption">
-<?php endif ?>
+					<!-- Change class depending on status -->
+					<?php if (get_field('status') == "Discounted"): ?>
+					<li class="passOption passOption--forSale">
+					<?php elseif (get_field('status') == "Sold Out"): ?> 
+					<li class="passOption passOption--soldOut">
+					<?php else: ?> 
+					<li class="passOption">
+					<?php endif ?>
 
-		<small class="passOption__date">
-			<?php echo get_field('start_date'); echo get_field('end_date')?>
-		</small>  
-		<h3 class="passOption__title">
-			<?php the_title(); ?><i class="fa fa-minus"></i>$<?php echo get_field('price');?>
-			<span class="passOption__note"><?php the_content(); ?></span>
-		</h3>
-	</li>
+						<small class="passOption__date">
+		          <?php echo get_field('start_date')." "; echo get_field('end_date')?>
+            </small>  
+            <h3 class="passOption__title">
+              <?php the_title(); ?> <i class="fa fa-minus"></i> $<?php echo get_field('price'); ?>
+              <span class="passOption__note"> <?php the_content(); ?> </span>
+            </h3>
+          </li>
 
-<?php
-	    endwhile;
-	endif; ?>
-</ul>
+				<?php
+				endwhile;
+			endif;
+			?>
+		</ul>
+	</div>
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -18,30 +18,29 @@
     <img src="../assets/images/ticketsPageGraphic.png">
   </div>
 
+
   <div class="passOptions">
     <ul>
-      <?php if ( have_posts() ) : ?>
-
-        <?php
+      <?php if ( have_posts() ) :
+        // Change Query to post_type of tickets rather than post (default)
+        $args = array('post_type'=>array('ticket'));
+        query_posts($args);
         // Start the loop.
-	$args = array('post_type'=>array('ticket'));
-	query_posts($args);
-
         while ( have_posts() ) : the_post();
         ?>
 
-
-	<?php if (get_field('status') == "Discounted"): ?>
-		<li class="passOption passOption--forSale">
-	<?php elseif (get_field('status') == "Sold Out"): ?> 
-		<li class="passOption passOption--soldOut">
-	<?php else: ?> 
-		<li class="passOption">
-	<?php endif ?>
-
+          <!-- Change class depending on status -->
+          <?php if (get_field('status') == "Discounted"): ?>
+          <li class="passOption passOption--forSale">
+          <?php elseif (get_field('status') == "Sold Out"): ?> 
+          <li class="passOption passOption--soldOut">
+          <?php else: ?> 
+          <li class="passOption">
+          <?php endif ?>
+          
             <small class="passOption__date">
-		<?php echo get_field('start_date')." "; echo get_field('end_date')?>
-	    </small>  
+		          <?php echo get_field('start_date')." "; echo get_field('end_date')?>
+            </small>  
             <h3 class="passOption__title">
               <?php the_title(); ?> <i class="fa fa-minus"></i> $<?php echo get_field('price'); ?>
               <span class="passOption__note"> <?php the_content(); ?> </span>
@@ -51,14 +50,13 @@
         <?php
         // End the loop.
         endwhile;
-
         // Previous/next page navigation.
         the_posts_pagination();
-
       endif;
       ?>
     </ul>
   </div>
+
 
   <div class="clearFloat">
   </div>
