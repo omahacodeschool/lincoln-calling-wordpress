8d81a63985b691d491b2be2936eb2fbc73d09b3e
Evin Grost <evin.grost@gmail.com>
2017-04-11 15:08:07 -0500

    Fixed nav bar.

--- header.php
+++ header.php
@@ -6,7 +6,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <link rel="stylesheet" type="text/css" href="<?php bloginfo("template_directory"); ?>/style.css" />
-<script type="text/javascript" src="<?php bloginfo("template_directory"); ?>/assets/scripts/navScript.js"></script>
 
 <?php wp_head(); ?>
 </head>
@@ -22,56 +21,36 @@
 
     <ul>
       
-      <?php wp_list_categories( array (
-        'title_li' => '',
-        'orderby' => 'id'
-      ) ); ?>
+      <li><a href="#">Buy Tickets</a></li>
 
-      <!-- <div class="commentedOut">
-        <li><a href="#">Buy Tickets</a></li>
+      <li>
+        <a href="#">Lineup <i class="fa fa-caret-down navDownArrow" aria-hidden="true"></i></a>
 
+        <ul class="subNav">
+
+        <?php wp_list_categories( array (
+          'title_li' => '',
+          'orderby' => 'id',
+          'child_of' => 24
+        ) ); ?>
+
+        </ul>
+      </li>
+
+      <li>
+        <a href="#">Get Involved <i class="fa fa-caret-down navDownArrow" aria-hidden="true"></i></a>
         
-        <li>
-          <a href="#">Lineup <i class="fa fa-caret-down navDownArrow" aria-hidden="true"></i></a>
+        <ul class="subNav">
 
-          <ul class="subNav">
-          
-            <li><a href="#">Music</a></li>
+          <?php wp_list_categories( array (
+            'title_li' => '',
+            'orderby' => 'id',
+            'child_of' => 25
+          ) ); ?>
 
-            <li><a href="#">Skate</a></li>
-
-            <li><a href="#">Fit</a></li>
-
-            <li><a href="#">Art</a></li>
-
-            <li><a href="#">Night Market</a></li>
-
-            <li><a href="#">Workshops</a></li>
-
-            <li><a href="#">YP Week</a></li>
-
-            <li><a href="#">Startup Week</a></li>
-
-            <li><a href="#">Sustainability</a></li>
-
-          </ul>
-
-        </li>
+        </ul>
 
 
-        
-        <li>
-          <a href="#">Get Involved <i class="fa fa-caret-down navDownArrow" aria-hidden="true"></i></a>
-
-          <ul>
-            
-            <li><a href="#">Link #1</a></li>
-
-            <li><a href="#">Link #2</a></li>
-
-            <li><a href="#">Link #3</a></li>
-          </ul>
-        </li>
 
         <li><a href="#">Updates</a></li>
 
@@ -80,7 +59,6 @@
         <li>
           <a href="#"><i class="fa fa-envelope-o newsletterLink" aria-hidden="true"></i>Newsletter</a>
         </li>
-      </div> -->
 
     </ul>
   </div>
