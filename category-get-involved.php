1da7a86e13c822f6c8995087cbde636515c01a34
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-12 10:08:22 -0500

    Rendered post details and included header and footer

--- category-get-involved.php
+++ category-get-involved.php
@@ -1,3 +1,5 @@
+<?php get_header(); ?>
+
 <!-- Gets the title of the category and prepends a string  -->
 <h1><?php single_cat_title('Info - '); ?></h1>
 <!-- Gets the description of the category  -->
@@ -8,10 +10,25 @@ if ( have_posts() ) :
     while ( have_posts() ) : the_post();
     ?>
 
-    <!-- Do stuff here -->
-    <p>Hi</p>
+	    <!-- Get Image -->
+	    <?php $image = get_field('article_image');
+	    if (!empty($image)):
+	    ?>
+			<img 
+				class="article__image"
+				src="<?php echo $image['url']; ?>" 
+				alt="<?php $image['alt']; ?>"
+			/>
+		<?php endif ?>
+
+
+		<!-- Get the actual post -->
+		<h3 class='article__title'><?php the_title(); ?></h3>
+	    <p class='article__body'><?php echo get_field('article_body'); ?></p>
 
     <?php endwhile;
     the_posts_pagination();
 endif;
 ?>
+
+<?php get_footer(); ?>
