abf544d99e9feb85c9caf328f146915ea4ae182e
Evin Grost <evin.grost@gmail.com>
2017-04-12 11:51:37 -0500

    Major progress on lineup template page.

--- category-lineup.php
+++ category-lineup.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
 
-<h1>Info - Artists</h1>
+<h1 class="templateTitle l-templateTitle">Info - <?php the_title() ?></h1>
+
+<p class="pageDescription"><?php echo strip_tags( category_description( $category_id ) ); ?></p>
 
 <?php get_footer(); ?>
