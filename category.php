7a4bcb82e437e73703fc82e41c1716424a4a11f3
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-18 10:49:33 -0500

    Created a list of links

--- category.php
+++ category.php
@@ -1,10 +1,20 @@
-<!-- Go through the posts of the current category -->
+<?php get_header(); ?>
+
+<h1 class="templateTitle l-templateTitle l-artistModalImage"><?php single_cat_title(); ?></h1>
+
+<ul>
 <?php if ( have_posts() ) :
 	while ( have_posts() ) : the_post(); ?>
 
-		<!-- Each post will be one artist for example -->
-		<?php the_title(); ?>
+	<li>
+		<a class="footerLinks__title l-block hr" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	</li>
 
 	<?php endwhile;
 endif;
 ?>
+</ul>
+
+
+
+<?php get_footer(); ?>
