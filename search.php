c5b6efa5d48d6e0d6376c9fae7d60c2e23732684
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-18 14:27:27 -0500

    Removed extra comments

--- search.php
+++ search.php
@@ -1,24 +1,4 @@
-<?php get_header(); ?>
-<?php 
-
-// Searches by all post, post types.
-//
-// And posts belonging to a child-category of lineup should link to the that child-cat.
-// Example:
-//
-// a link to Flight of the Conchords belongs to the category Music, and Lineup.
-// The href will be to .../lineup/music/ aka the category archive page
-
-// Create perfect links 
-// --------------------
-//
-// if (posts categories include lineup){
-//		change href to immediate cat
-// }
-// else{
-//		use the_permalink();
-// }
-
+<?php get_header();
 
 if ( have_posts() ):
 	while ( have_posts() ) : the_post(); ?>
@@ -40,5 +20,5 @@ if ( have_posts() ):
 		<?php endwhile;
 	the_posts_pagination();
 endif;
-?>
-<?php get_footer(); ?>
+
+get_footer(); ?>
