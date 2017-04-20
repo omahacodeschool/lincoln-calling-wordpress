e34d0317b71ea8c66919c138d549369cce568980
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-20 11:03:42 -0500

    removed info

--- search.php
+++ search.php
@@ -1,11 +1,11 @@
-<?php get_header();
+<?php get_header(); ?>
+<h1 class="templateTitle l-templateTitle l-artistModalImage">Search results</h1>
+<ul>
 
-if ( have_posts() ):
+<?php if ( have_posts() ):
 	while ( have_posts() ) : the_post(); ?>
 
-
-		<li>
-		
+		<li>		
 		<?php if ( strpos( get_the_category_list(), "Lineup" )) {
 			$slug = get_the_category()[1]->slug; ?>
 			<a class="footerLinks__title hr l-block" href="<?php echo $slug ?>"><?php the_title(); ?></a>
@@ -13,12 +13,10 @@ if ( have_posts() ):
 		else{ ?>
 			<a class="footerLinks__title hr l-block" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		<?php } ?>
-
 		</li>
 
+	<?php endwhile;
+endif; ?>
 
-		<?php endwhile;
-	the_posts_pagination();
-endif;
-
-get_footer(); ?>
+</ul>
+<?php get_footer(); ?>
