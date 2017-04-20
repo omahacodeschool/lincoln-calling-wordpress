918b22dd926db7291d0c9967621b6712b4171110
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-20 11:04:42 -0500

    Indent

--- category.php
+++ category.php
@@ -1,20 +1,15 @@
 <?php get_header(); ?>
-
 <h1 class="templateTitle l-templateTitle l-artistModalImage"><?php single_cat_title(); ?></h1>
-
 <ul>
+
 <?php if ( have_posts() ) :
 	while ( have_posts() ) : the_post(); ?>
-
-	<li>
-		<a class="footerLinks__title l-block hr" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-	</li>
-
+		<li>
+			<a class="footerLinks__title l-block hr" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</li>
 	<?php endwhile;
 endif;
 ?>
+
 </ul>
-
-
-
 <?php get_footer(); ?>
