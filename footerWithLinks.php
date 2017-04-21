d7d715e57841493d0569426834ca8c3315079ff2
Evin Grost <evin.grost@gmail.com>
2017-04-21 10:19:09 -0500

    Making tweaks for the live site.

--- footerWithLinks.php
+++ footerWithLinks.php
@@ -1,4 +1,37 @@
   <div class="footerContainer">
+    <!-- <div class="l-footerLinksContainer">
+      <div class="footerLinks l-footerLinks">
+        <h4 class="footerLinks__title">LINEUP</h4>
+        <ul>
+          <li><a href="/category/lineup/music">Artists</a></li>
+          <li><a href="#">Conference</a></li>
+          <ul class="footerLinks__sublinks">
+            <li><a href="/category/lineup/yp-week">Yp Week</a></li>
+            <li><a href="/category/lineup/startup-week">Startup Week</a></li>
+            <li><a href="/category/lineup/fit-week">Fit Week</a></li>
+          </ul>
+          <li><a href="/category/lineup/night-market">Night Market</a></li>
+          <li><a href="#">2016 Lineup</a></li>
+        </ul>
+      </div>
+      <div class="footerLinks l-footerLinks">
+        <h4 class="footerLinks__title">UPDATES</h4>
+        <ul>
+        </ul>
+      </div>
+      <div class="footerLinks l-footerLinks">
+        <h4 class="footerLinks__title">FEST INFO</h4>
+        <ul>
+          <li><a href="/index">Ticket Info</a></li>
+        </ul>
+      </div>
+      <div class="footerLinks l-footerLinks">
+        <h4 class="footerLinks__title">GET INVOLVED</h4>
+        <ul>
+          <li><a href="/get-involved/volunteer">Volunteer</a></li>
+        </ul>
+      </div>
+    </div> -->
 
     <form action="/" class="searchForm">
       <input class="searchBar" type="text" name="s" id="s" placeholder="SEARCH...">
@@ -27,19 +60,19 @@
       <li>
         <div class="credit l-credit">
           <p>Presented by:</p>
-          <a href="https://www.allocommunications.com/"><img src="<?php bloginfo("template_directory"); ?>/assets/images/Allo.png"></a>
+          <img src="<?php bloginfo("template_directory"); ?>/assets/images/Allo.png">
         </div>
       </li>
       <li>
         <div class="credit ocsLogo l-credit">
           <p>Site by:</p>
-          <a href="http://www.omahacodeschool.com"><img src="<?php bloginfo("template_directory"); ?>/assets/images/OCS.png"></a>
+          <img src="<?php bloginfo("template_directory"); ?>/assets/images/OCS.png">
         </div>
       </li>
       <li>
         <div class="credit l-credit">
           <p>Produced by:</p>
-          <a href="http://www.hearnebraska.org"><img src="<?php bloginfo("template_directory"); ?>/assets/images/HearNE.png"></a>
+          <img src="<?php bloginfo("template_directory"); ?>/assets/images/HearNE.png">
         </div>
       </li>
     </ul>
