0ac5473e7c3424e36f44e8851f67fb2c4b1e58b4
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-21 14:07:45 -0500

    Fix

--- page.php
+++ page.php
@@ -1,14 +1,20 @@
 <?php get_header(); ?>
 
-<div class="templateBody">
-	<div class="article">
-		<h3 class='templateTitle article__title l-article__title'>
-			<?php the_title(); ?>
-		</h3>
-	    <p class='pageBody'>
-		    <?php the_content(); ?>	
-	    </p>
-    </div>
-</div>
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	<div class="templateBody">
+		<div class="article">
+			<h3 class='templateTitle article__title l-article__title'>
+				<?php the_title(); ?>
+			</h3>
+		    <p class='pageBody'>
+			    <?php the_content(); ?>	
+		    </p>
+	    </div>
+	</div>
+
+<?php endwhile; else : ?>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 
 <?php get_footer(); ?>
